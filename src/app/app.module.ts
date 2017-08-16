b720b6e708c7a547efb12843a97cabf9be6a78c0
mtclark0518 <mtclark0518@gmail.com>
2017-08-16 14:14:01 -0600

    quarter of the way through http tutorial section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
-import { BrowserModule }     from '@angular/platform-browser';
+import { BrowserModule }        from '@angular/platform-browser';
 import { NgModule }             from '@angular/core';
 import { FormsModule }          from '@angular/forms';
+import { HttpModule }           from '@angular/http';
+import {AppRoutingModule}       from './app-routing.module';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data-service';
 
 import { AppComponent }         from './app.component';
 import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
 import { DashboardComponent }   from './dashboard/dashboard.component';
 import { HeroesComponent }      from './heroes/heroes.component';
 import { HeroService }          from './services/hero/hero.service';
-import {AppRoutingModule}       from './app-routing.module';
 
 
 @NgModule({
@@ -20,6 +24,8 @@ import {AppRoutingModule}       from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   providers: [HeroService],
